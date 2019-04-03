249f192479519e95588a599e10602f9b6336f049
shanjathurshan <jathurshanm3@gmail.com>
2019-04-04 00:36:09 +0530

    fernweh

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 var host       = require('./models/host');
 
+
 // app/routes.js
 module.exports = function(app, passport) {
 
@@ -17,6 +18,39 @@ module.exports = function(app, passport) {
     app.get('/contact', function(req, res) {
         res.render('contact.ejs'); // load the index.ejs file
     });
+    app.get('/auto', function(req, res) {
+        res.render('auto.ejs'); // load the index.ejs file
+    });
+
+    ///////////////////////////////////// get data/////
+    // app.get('/detailsArray/:id', function (req, res) {
+    //     let id = req.params.id;
+    //     axios.get('https://api.thetvdb.com/episodes/'+id)
+    //         .then(response => {
+    //             let data = response.data;
+    //             console.log(data)
+    //             res.send(JSON.stringify(data))
+    //         })
+    //         .catch(error => {
+    //             let err = error;
+    //             res.send(JSON.stringify(err))
+    //         });
+    // });
+
+    app.get('/getBikes', (req, res) => {
+      host.find({}, function(err, result) {
+        if(err) {
+          res.send(err);
+        } else {
+         res.send(result)
+        }
+
+
+      });
+    });
+
+    ////////////////////////////////////////////////////
+
 
     app.get('/about', function(req, res) {
         res.render('about.ejs', {
@@ -262,7 +296,7 @@ module.exports = function(app, passport) {
                                       from: 'fernwehrental@gmail.com', // sender address
                                       to: user.local.email, // list of receivers
                                       subject: 'Fernweh Registration Details', // Subject line
-                                      html: '<div style="background-color:#eeeeef;padding:50px 0"><table style="width:540px" border="0" cellspacing="0" cellpadding="0" align="center"><tbody><tr><td style="padding:10px 30px 10px 30px" align="center" bgcolor="#33333e"><h1 style="color:#fff">Hi ' + user.local.lastname + ',</h1><h2 style="color:#fff">Your order has been palced successfully. We will contact you about the order as soon as possible. </h2></td></tr><tr><td bgcolor="#ffffff" style="padding:40px 30px 10px 30px"><table border="0" cellpadding="0" cellspacing="0" width="100%"><tbody><tr><td width="260" valign="top"><table border="0" cellpadding="0" cellspacing="0" width="100%"><tbody><tr><td>This is a system generated email and please do not reply. For more information please contact to +94 77 851 3071 .</td></tr><h3 style="text-align:center;"> Two wheeler Order Deatils </h3><tr><td style="padding:10px 0 0 0">Two wheeler id: '+newhost.carId+  '</td></tr><tr><td style="padding:10px 0 0 0">First name: '+user.local.firstname+ '</td></tr><tr><td style="padding:10px 0 0 0">Last name: '+ user.local.lastname+ '</td></tr><tr><td style="padding:10px 0 0 0">Email: '+newhost.email+ '</td></tr><tr><td style="padding:10px 0 0 0">Phone number: '+newhost.phonenumber+ '</td></tr><tr><td style="padding:10px 0 0 0">Address: '+newhost.address+ '</td></tr><tr><td style="padding:10px 0 0 0">Nic/passport: '+newhost.nic+ '</td></tr><tr><td style="padding:10px 0 0 0">From this date: '+newhost.fdate+ '</td></tr><tr><td style="padding:10px 0 0 0">Until this date: ' +newhost.udate+ '</td></tr></tbody></table></td></tr></tbody></table></td></tr><tr><td align="center" bgcolor="#fff"><h3>Thank you<br/>Fernweh Team</h3></td></tr><tr><td style="background-color:#33333d;padding:20px 30px 20px 30px"><table border="0" width="100%" cellspacing="0" cellpadding="0"><tbody><tr><td style="font-family:Arial,sans-serif;font-size:14px;color:#fff">® Fernweh, 2019</td></tr></tbody></table></td></tr></tbody></table></div>' // plain text body
+                                      html: '<div style="background-color:#eeeeef;padding:50px 0"><table style="width:540px" border="0" cellspacing="0" cellpadding="0" align="center"><tbody><tr><td style="padding:10px 30px 10px 30px" align="center" bgcolor="#33333e"><h1 style="color:#fff">Hi ' + user.local.lastname + ',</h1><h2 style="color:#fff">Your order has been placed successfully. We will contact you about the order as soon as possible. </h2></td></tr><tr><td bgcolor="#ffffff" style="padding:40px 30px 10px 30px"><table border="0" cellpadding="0" cellspacing="0" width="100%"><tbody><tr><td width="260" valign="top"><table border="0" cellpadding="0" cellspacing="0" width="100%"><tbody><tr><td>This is a system generated email and please do not reply. For more information please contact to +94 77 851 3071 .</td></tr><h3 style="text-align:center;"> Two wheeler Order Deatils </h3><tr><td style="padding:10px 0 0 0">Two wheeler id: '+newhost.carId+  '</td></tr><tr><td style="padding:10px 0 0 0">First name: '+user.local.firstname+ '</td></tr><tr><td style="padding:10px 0 0 0">Last name: '+ user.local.lastname+ '</td></tr><tr><td style="padding:10px 0 0 0">Email: '+newhost.email+ '</td></tr><tr><td style="padding:10px 0 0 0">Phone number: '+newhost.phonenumber+ '</td></tr><tr><td style="padding:10px 0 0 0">Address: '+newhost.address+ '</td></tr><tr><td style="padding:10px 0 0 0">Nic/passport: '+newhost.nic+ '</td></tr><tr><td style="padding:10px 0 0 0">From this date: '+newhost.fdate+ '</td></tr><tr><td style="padding:10px 0 0 0">Until this date: ' +newhost.udate+ '</td></tr></tbody></table></td></tr></tbody></table></td></tr><tr><td align="center" bgcolor="#fff"><h3>Thank you<br/>Fernweh Team</h3></td></tr><tr><td style="background-color:#33333d;padding:20px 30px 20px 30px"><table border="0" width="100%" cellspacing="0" cellpadding="0"><tbody><tr><td style="font-family:Arial,sans-serif;font-size:14px;color:#fff">® Fernweh, 2019</td></tr></tbody></table></td></tr></tbody></table></div>' // plain text body
                                     };
                                     transporter.sendMail(mailOptions, function (err, info) {
                                        if(err)
