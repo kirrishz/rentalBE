18e7897975cf9c964b5e0643b9e1962de46706cc
shanjathurshan <jathurshanm3@gmail.com>
2019-04-03 05:45:27 +0530

    fernweh

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://uki4:uki1234@ds139775.mlab.com:39775/notable_uki4' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://fernwehrental:fernweh1998@ds044979.mlab.com:44979/fernweh' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
