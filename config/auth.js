e2a93a1b1e8cecc695a547b312c2d0e806c04d12
kirrishz <s.kirishanth97@gmail.com>
2019-07-20 16:30:41 +0530

    frenweh

--- config/auth.js
+++ config/auth.js
@@ -1,24 +1,27 @@
+
+// config/auth.js
+
 // expose our config directly to our application using module.exports
 module.exports = {
 
-  'facebookAuth' : {
-      'clientID'      : '357977017943125', // your App ID
-      'clientSecret'  : 'e3427977539e1f6348e0652470639142',
-      'callbackURL'   : 'https://gleefly.herokuapp.com/auth/facebook/callback',
-      'profileURL'    : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
-      'profileFields' : ['id', 'emails', 'name'] // For requesting permissions from Facebook API
-  },
+    'facebookAuth' : {
+        'clientID'      : '333216393990838', // your App ID
+        'clientSecret'  : '8d1cea1d8ce084aa1f1968c4fc6add6e', // your App Secret
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback',
+        'profileURL'    : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
+        'profileFields' : ['id', 'email', 'name'] // For requesting permissions from Facebook API
+    },
 
-  'twitterAuth' : {
-      'consumerKey'       : 'bJqg5WcNCFimqKG0G0476u12q',
-      'consumerSecret'    : 'OIEyUKjt93t3Kva6A7VfLKWCpIFnYu7QPMvYudHDfaO6JAbyAa',
-      'callbackURL'       : 'http://gleefly.herokuapp.com/auth/twitter/callback'
-  },
+    'twitterAuth' : {
+        'consumerKey'       : 'your-consumer-key-here',
+        'consumerSecret'    : 'your-client-secret-here',
+        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+    },
 
     'googleAuth' : {
-        'clientID'      : '719244576802-vb1l8je58jr8reg3ocgnp056o4sucohu.apps.googleusercontent.com',
-        'clientSecret'  : '6JDLMcKZhQAYtbvOZpTr-P8C',
-          'callbackURL'   : 'http://gleefly.herokuapp.com/auth/google/callback'
+        'clientID'      : 'your-secret-clientID-here',
+        'clientSecret'  : 'your-client-secret-here',
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
     }
 
 };
